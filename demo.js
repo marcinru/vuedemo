0673fa87297a716526ea864c11a5f86c1c875202
Marcin Rudzki <mar.rudzki@gmail.com>
2018-05-02 08:59:35 +0200

    Add addInvoice() method

--- demo.js
+++ demo.js
@@ -3,5 +3,18 @@ new Vue({
     data: {
         creditorNumber: 123456,
         invoices: []
+    },
+    methods: {
+        addInvoice() {
+            this.invoices.push({
+                number: 123,
+                text: 'description',
+                date: '2018-05-02',
+                duedate: '2018-05-22',
+                currency: 'EUR',
+                amount: 123.45,
+                attachment: ''
+            })
+        }
     }
 })
