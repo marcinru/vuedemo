0591f872a9b1cd29f4c35b6f15664fab3e5ddf86
Marcin Rudzki <mar.rudzki@gmail.com>
2018-05-03 17:23:27 +0200

    Add form data

--- demo.js
+++ demo.js
@@ -1,23 +1,33 @@
 new Vue({
     el: '#app',
     data: {
-        creditorNumber: 123456,
+        creditorNumber: '',
+        number: '',
+        text: '',
+        date: '',
+        duedate: '',
+        currency: 'NOK',
+        amount: 0,
+        attachment: '',
         invoices: []
     },
     methods: {
         addInvoice() {
             this.invoices.push({
-                number: 123,
-                text: 'description',
-                date: '2018-05-02',
-                duedate: '2018-05-22',
-                currency: 'NOK',
-                amount: 123.45,
-                attachment: ''
+                number: this.number,
+                text: this.text,
+                date: this.date,
+                duedate: this.duedate,
+                currency: this.currency,
+                amount: this.amount,
+                attachment: this.attachment
             })
         },
         deleteInvoice() {
             this.invoices.pop()
+        },
+        resetForm() {
+            //
         }
     }
 })
