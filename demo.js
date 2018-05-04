105fb53a19a1aa6f5b4f31450a99631fd0c04576
Marcin Rudzki <mar.rudzki@gmail.com>
2018-05-04 18:35:43 +0200

    Add v-if=invoice.attachment and :title=invoice.attachment

--- demo.js
+++ demo.js
@@ -11,11 +11,6 @@ new Vue({
         attachment: '',
         invoices: []
     },
-    computed: {
-        hasAttachment: function() {
-            return this.attachment.length > 0
-        }
-    },
     methods: {
         addInvoice() {
             this.invoices.push({
